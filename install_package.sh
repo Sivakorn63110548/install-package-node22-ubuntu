421aa63361acbc9f088d804e19c085f4613e1227
Sivakorn Tanyupak <121595270+Sivakorn63110548@users.noreply.github.com>
2025-08-20 09:49:58 +0700

    Add files via upload

---
+++ install_package.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Updating package list..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing Curl..."
+sudo apt install curl -y
+
+echo "Installing Git..."
+sudo apt install git -y
+git --version
+
+echo "Installing Tailscale..."
+curl -fsSL https://tailscale.com/install.sh | sh
+
+echo "Installing NVM..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+echo "Installing Node.js 22..."
+nvm install 22
+nvm use 22
+node -v
+
+echo "Installing Yarn..."
+npm install -g yarn
+yarn -v
+
+echo "Installing PM2..."
+npm install -g pm2
+pm2 -v
+
+echo "Installation complete!"
